f8efb67da15ffda9406eb8b4bf8ff0bc079fa66a
Ben van Basten <ben.vanbasten@nelen-schuurmans.nl>
2024-12-01 21:59:59 +0100

    refactor

--- threedi_mi_utils/news/news_injector.py
+++ threedi_mi_utils/news/news_injector.py
@@ -1,74 +1,97 @@
 import json
-from datetime import datetime as Datetime
+import re
 from pathlib import Path
-from typing import List
+from typing import Any, Dict, List
 
-from qgis.core import QgsNewsFeedParser
-from qgis.PyQt.QtCore import QUrl
+from qgis.core import QgsSettings
+from qgis.PyQt.QtCore import QDateTime, Qt
 
-__all__ = ["QgsNewsInjector"]
+__all__ = ["QgsNewsSettingsInjector"]
 
 
-class QgsNewsInjector:
+class QgsNewsSettingsInjector:
     """
         A hack to be able to add custom news items to Qgis news feed window.
         https://gis.stackexchange.com/questions/342147/adding-own-items-into-qgis-3-10-news-feed/342218#342218
 
-        This class (mis)uses QgsNewsFeedParser to parse the json feed and update the settings. Don't use fetch(), it
-        will retrieve the online feed.
+        We'll add additional news item (with a very high key) in the settings...
     """
-    # We still need this url as the entry in the setting is based on this and (hardcoded) used for later lookup
-    feed_url = "https://feed.qgis.org/"
+    # The url is hardcoded, so we need this settings path.
+    settings_path = "app/news-feed/items/httpsfeedqgisorg/entries/items/"
     # To distinguish custom news items from QGIS news items, we start custom items at this (extremely high) offset
-    pk_offset = 10000000
-
-    def __init__(self):
-        self.update()
+    key_offset = 10000000
+    regex = r"^app/news-feed/items/httpsfeedqgisorg/entries/items/([0-9]*)/*"
 
     def load(self, file_path: Path) -> bool:
         """Loads all news items from a provided JSON file"""
         with open(file_path, 'r') as file:
-            self.update()
-            existing_entries = self.parser.entries()
 
-            # Check whether all items > pk_offset and don't already exists
+            # Check whether all items > key_offset
             entries = json.load(file)
-            filtered_entries = []
             for entry in entries:
-                pk = entry["pk"]
-                if pk < QgsNewsInjector.pk_offset:
+                pk = entry["key"]
+                if pk < QgsNewsSettingsInjector.key_offset:
                     continue
 
-                if next((x for x in existing_entries if x.key == pk), None):
-                    continue
+                if entry["expiry"]:
+                    entry["expiry"] = QDateTime.fromString(entry["expiry"], Qt.ISODate)
 
-                if entry["publish_from"]:
-                    entry["publish_from"] = Datetime.fromisoformat(entry["publish_from"]).timestamp()
-                if entry["publish_to"]:
-                    entry["publish_to"] = Datetime.fromisoformat(entry["publish_to"]).timestamp()
-                filtered_entries.append(entry)
-
-            self.add_items(json.dumps(filtered_entries))
-            self.update()
-
+                self.add_item(entry)
         return True
 
-    def update(self) -> None:
-        """Reads all the entries from the settings and prunes expired entries,
-          including entries with the same id"""
-        # invokes readStoredEntries()
-        self.parser = QgsNewsFeedParser(QUrl(QgsNewsInjector.feed_url))
+    def items(self) -> List[Dict[str, Any]]:
+        """Returns the list of custom news items in the settings, useful for testing"""
+        all_news_settings_entries = \
+            {t[0:t.rfind("/")] for t in QgsSettings().allKeys() if t.startswith(QgsNewsSettingsInjector.settings_path)}
+        keys = set()
+        for news_setting_entry in all_news_settings_entries:
+            m = re.search(QgsNewsSettingsInjector.regex, news_setting_entry)
+            key = int(m.groups()[0])
+            if key >= QgsNewsSettingsInjector.key_offset:
+                keys.add(key)
 
-    def items(self) -> List[QgsNewsFeedParser.Entry]:
-        """Returns the list of news items"""
-        return self.parser.entries()
+        result = []
+        for key in keys:
+            entry = {}
+            entry_path = QgsNewsSettingsInjector.settings_path + str(key)
+            entry["title"] = QgsSettings().value(entry_path + "/title")
+            entry["image-url"] = QgsSettings().value(entry_path + "/image-url")
+            entry["content"] = QgsSettings().value(entry_path + "/content")
+            entry["link"] = QgsSettings().value(entry_path + "/link")
+            entry["sticky"] = QgsSettings().value(entry_path + "/sticky")
+            entry["expiry"] = QgsSettings().value(entry_path + "/expiry")
+            entry["key"] = key
+            result.append(entry)
+
+        return result
 
     def clear(self) -> None:
-        """Removes all news items, including QGIS items, useful for testing"""
-        self.parser.dismissAll()
+        """Removes all custom news items from settings, useful for testing"""
+        all_news_settings_entries = \
+            {t for t in QgsSettings().allKeys() if t.startswith(QgsNewsSettingsInjector.settings_path)}
 
-    def add_items(self, items_string: str) -> None:
-        """Add items, if there are expired items, these are only removed after
-         a call to update()"""
-        # This stores entries in member variable AND settings
-        self.parser.onFetch(items_string)
+        for news_setting_entry in all_news_settings_entries:
+            m = re.search(QgsNewsSettingsInjector.regex, news_setting_entry)
+            key = int(m.groups()[0])
+            if key >= QgsNewsSettingsInjector.key_offset:
+                QgsSettings().remove(news_setting_entry)
+
+    def add_item(self, entry: Dict[str, Any]) -> bool:
+        """Add items to settings, but only if they are not expired and do not already exist"""
+        if entry["key"] is None:
+            return False
+
+        if entry["expiry"] and entry["expiry"] < QDateTime.currentDateTime():
+            return False
+
+        entry_path = QgsNewsSettingsInjector.settings_path + str(entry["key"])
+        if QgsSettings().contains(entry_path + "/title"):  # contains() requires complete entry
+            return False
+
+        QgsSettings().setValue(entry_path + "/title", entry["title"])
+        QgsSettings().setValue(entry_path + "/image-url", entry["image-url"])
+        QgsSettings().setValue(entry_path + "/content", entry["content"])
+        QgsSettings().setValue(entry_path + "/link", entry["link"])
+        QgsSettings().setValue(entry_path + "/sticky", entry["sticky"])
+        QgsSettings().setValue(entry_path + "/expiry", entry["expiry"])
+        return True
--- threedi_mi_utils/tests/news_test.py
+++ threedi_mi_utils/tests/news_test.py
@@ -1,64 +1,47 @@
 from pathlib import Path
-from typing import Any
 
 import pytest
+from qgis.PyQt.QtCore import QDateTime, Qt
 
-from threedi_mi_utils.news import QgsNewsInjector
-
-
-def none_to_null(value: Any) -> str:
-    return value if value is not None else "null"
+from threedi_mi_utils.news import QgsNewsSettingsInjector
 
 
-def compare(entry, test_item):
-    assert entry.key == test_item.pk
-    assert ((entry.expiry.toSecsSinceEpoch() if entry.expiry.isValid() else None)
-            == int(test_item.publish_to))
-    assert entry.title == test_item.title
-    assert entry.content == test_item.content
-    assert entry.imageUrl == test_item.image
-    assert entry.link.toString() == test_item.url
-    assert entry.sticky == test_item.sticky
-
-
-class TestItem():
-
-    __test__ = False
-
-    def __init__(self, pk: int, publish_from: int, publish_to: int, title: str,
-                image: str, content: str, url: str, sticky: bool):
-        self.pk = pk
-        self.publish_from = publish_from
-        self.publish_to = publish_to
-        self.title = title
-        self.image = image
-        self.content = content
-        self.url = url
-        self.sticky = sticky
-
-    def __repr__(self):
-        return f'[{{"pk": {none_to_null(self.pk)}, "publish_from": {none_to_null(self.publish_from)}, "publish_to": {none_to_null(self.publish_to)}, \
-"title": "{none_to_null(self.title)}", "image": "{none_to_null(self.image)}", "content": "{none_to_null(self.content)}", "url": "{none_to_null(self.url)}", \
-"sticky": {none_to_null(str(self.sticky).lower())}}}]'
-    
 @pytest.fixture()
 def news_injector():
-    injector = QgsNewsInjector()
+    injector = QgsNewsSettingsInjector()
     yield injector
     injector.clear()
 
+
 @pytest.fixture()
 def news_item():
-    return TestItem(pk=4, publish_from=1557073748.13, publish_to=2557073748.13, title="test title", image="", content="<p>test content</p>", url="bla", sticky=False)
+    return {
+        "key": 10000005,
+        "expiry": QDateTime.fromString("2999-01-01", Qt.ISODate),
+        "title": "test title",
+        "image-url": "",
+        "content": "<p>test content</p>",
+        "link": "bla",
+        "sticky": False
+    }
+
 
 @pytest.fixture()
 def sticky_news_item(news_item):
-    news_item.sticky = True
+    news_item["sticky"] = True
     return news_item
 
 @pytest.fixture()
 def expired_news_item():
-    return TestItem(pk=5, publish_from=None, publish_to=17073748.13, title="test title", image="", content="<p>test content</p>", url="test", sticky=False)
+    return {
+        "key": 10000006,
+        "expiry":  QDateTime.fromString("1999-01-01", Qt.ISODate),
+        "title": "test title",
+        "image-url": "",
+        "content": "<p>test content</p>",
+        "link": "test",
+        "sticky": False
+    }
 
 @pytest.fixture()
 def data_folder():
@@ -68,41 +51,26 @@ def test_no_news_at_start(news_injector):
     assert len(news_injector.items()) == 0
 
 def test_add_item(news_injector, news_item):
-    news_injector.add_items(str(news_item))
-    assert len(news_injector.items()) == 1
-    news_injector.update()
+    assert news_injector.add_item(news_item)
     assert len(news_injector.items()) == 1
     entry = news_injector.items()[0]
-    compare(entry, news_item)
+    assert entry == news_item
 
 def test_add_sticky_item(news_injector, sticky_news_item):
-    news_injector.add_items(str(sticky_news_item))
-    assert len(news_injector.items()) == 1
-    news_injector.update()
+    assert news_injector.add_item(sticky_news_item)
     assert len(news_injector.items()) == 1
     entry = news_injector.items()[0]
-    assert entry.sticky == True
-    compare(entry, sticky_news_item)
+    assert entry["sticky"] == True
+    assert entry == sticky_news_item
     
 def test_add_expired_item(news_injector, expired_news_item):
-    # Expired items are added the first time ...
-    news_injector.add_items(str(expired_news_item))
-    assert len(news_injector.items()) == 1
-    entry = news_injector.items()[0]
-    compare(entry, expired_news_item)
-    news_injector.update()  # ... but are pruned at an update()
+    assert not news_injector.add_item(expired_news_item)
     assert len(news_injector.items()) == 0
-
-def test_add_corrupt_item(news_injector):
-    news_injector.add_items('thisisnotvalidjson[{"pk": 6, "publish_from": 1557073]')
-    assert len(news_injector.items()) == 0
-
+ 
 def test_item_twice(news_injector, news_item):
-    news_injector.add_items(str(news_item))
+    assert news_injector.add_item(news_item)
     assert len(news_injector.items()) == 1
-    news_injector.add_items(str(news_item))
-    assert len(news_injector.items()) == 2
-    news_injector.update()  # Double items are pruned at an update
+    assert not news_injector.add_item(news_item)
     assert len(news_injector.items()) == 1
 
 def test_load_items_expired(news_injector, data_folder):
@@ -110,21 +78,20 @@ def test_load_items_expired(news_injector, data_folder):
     assert news_injector.load(data_folder / "feed_expired.json")
     # one entry is expired, so 3 items
     assert len(news_injector.items()) == 3
-    news_injector.items()[0].key == 10000004
-    news_injector.items()[1].key == 10000005
-    news_injector.items()[1].key == 10000006
+    news_injector.items()[0]["key"] == 10000004
+    news_injector.items()[1]["key"] == 10000005
+    news_injector.items()[1]["key"] == 10000006
 
 def test_load_items_double(news_injector, data_folder):
     assert news_injector.load(data_folder / "feed_double.json")
     # double ids, so 2 items
     assert len(news_injector.items()) == 2
-    news_injector.items()[0].key == 10000004
-    news_injector.items()[1].key == 10000005
-    news_injector.items()[1].title == "title3"  # last double item remains
-    news_injector.items()[1].link == "link3"  # last double item remains
+    news_injector.items()[0]["key"] == 10000004
+    news_injector.items()[1]["key"] == 10000005
+    news_injector.items()[1]["title"] == "title2"  # first double item remains
+    news_injector.items()[1]["link"] == "link2"  # first double item remains
 
 def test_load_items_too_small_pk(news_injector, data_folder):
     assert news_injector.load(data_folder / "feed_too_small_pk.json")
-    # double ids, so 2 items
     assert len(news_injector.items()) == 1
-    news_injector.items()[0].key == 10000005
+    news_injector.items()[0]["key"] == 10000005
