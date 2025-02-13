606d4cb61f21841b27135a1647f733cc652f8c85
elisalle <eli.salle@nelen-schuurmans.nl>
2025-02-13 16:55:48 +0100

    Preparing release 0.1.6.dev0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="threedi-mi-utils",
-    version="0.1.5",
+    version="0.1.6.dev0",
     packages=find_packages(),
     url="https://github.com/nens/threedi-mi-utils",
     license="GNU General Public License v3.0",
